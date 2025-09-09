6d55d7cb61991b76cdc39a77015c0a7559ef79e8
fubuki <fubukishirakami117@gmail.com>
2025-09-09 10:04:50 +0800

    Update embedLogger.js

--- src/utils/embedLogger.js
+++ src/utils/embedLogger.js
@@ -1,1 +1,26 @@
 
+const { EmbedBuilder } = require("discord.js");
+
+function createLogEmbed({ color, title, description, fields = [] }) {
+  return new EmbedBuilder()
+    .setColor(color)
+    .setTitle(title)
+    .setDescription(description || null)
+    .addFields(fields)
+    .setTimestamp();
+}
+
+// 🎨 Warna standar
+const colors = {
+  delete: "Red",
+  edit: "Orange",
+  roleAdd: "Green",
+  roleRemove: "Red",
+  memberLeave: "Grey",
+  ban: "DarkRed",
+  unban: "Blue",
+  timeout: "Purple",
+};
+
+module.exports = { createLogEmbed, colors };
+
