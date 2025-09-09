6dba95322f1d09cf45772ad1364d3daf300b3c55
fubuki <fubukishirakami117@gmail.com>
2025-09-09 10:52:59 +0800

    Update embedLogger.js

--- src/embedLogger.js
+++ src/embedLogger.js
@@ -1,5 +1,12 @@
 const { EmbedBuilder } = require("discord.js");
 
+/**
+ * Membuat embed log
+ * @param {string} title - Judul embed
+ * @param {string} description - Isi embed
+ * @param {number} color - Warna embed (default: abu-abu gelap)
+ * @returns {EmbedBuilder}
+ */
 function embedLogger(title, description, color = 0x2f3136) {
   return new EmbedBuilder()
     .setTitle(title)
